44878063f1f264936760c9060a407e83e1e88052
Joao Monteiro <joaomonteirof@gmail.com>
2018-05-28 19:03:27 -0400

    generative model for frames

---
+++ cup_generator/data_load.py
@@ -0,0 +1,30 @@
+import h5py
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import os
+
+
+class Loader(Dataset):
+
+	def __init__(self, hdf5_name):
+		super(Loader, self).__init__()
+		self.hdf5_name = hdf5_name
+
+		open_file = h5py.File(self.hdf5_name, 'r')
+		self.length = len(open_file['data'])
+		open_file.close()
+
+		self.open_file = None
+
+	def __getitem__(self, index):
+
+		if not self.open_file: self.open_file = h5py.File(self.hdf5_name, 'r')
+
+		scene = self.open_file['data'][index]
+		idx = np.random.randint(scene.shape[0])
+
+		return torch.from_numpy(scene[idx]).unsqueeze(0).float()
+
+	def __len__(self):
+		return self.length
---
+++ cup_generator/model.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Generator(torch.nn.Module):
+	def __init__(self):
+		super(Generator, self).__init__()
+
+		#linear layer
+		self.linear = torch.nn.Sequential()
+
+		linear = nn.Linear(100, 2*2*512)
+
+		self.linear.add_module('linear', linear)
+
+		# Initializer
+		nn.init.normal(linear.weight, mean=0.0, std=0.02)
+		nn.init.constant(linear.bias, 0.0)
+
+		# Batch normalization
+		bn_name = 'bn0'
+		self.linear.add_module(bn_name, torch.nn.BatchNorm1d(2*2*512))
+
+		# Activation
+		act_name = 'act0'
+		self.linear.add_module(act_name, torch.nn.ReLU())
+
+		# Hidden layers
+		num_filters = [256, 128, 64]
+		self.hidden_layer = torch.nn.Sequential()
+		for i in range(3):
+			# Deconvolutional layer
+			if i == 0:
+				deconv = nn.ConvTranspose2d(512, num_filters[i], kernel_size=4, stride=2, padding=1)
+			else:
+				deconv = nn.ConvTranspose2d(num_filters[i - 1], num_filters[i], kernel_size=4, stride=2, padding=1)
+
+			deconv_name = 'deconv' + str(i + 1)
+			self.hidden_layer.add_module(deconv_name, deconv)
+
+			# Initializer
+			nn.init.normal(deconv.weight, mean=0.0, std=0.02)
+			nn.init.constant(deconv.bias, 0.0)
+
+			# Batch normalization
+			bn_name = 'bn' + str(i + 1)
+			self.hidden_layer.add_module(bn_name, torch.nn.BatchNorm2d(num_filters[i]))
+
+			# Activation
+			act_name = 'act' + str(i + 1)
+			self.hidden_layer.add_module(act_name, torch.nn.ReLU())
+
+		# Output layer
+		self.output_layer = torch.nn.Sequential()
+		# Deconvolutional layer
+		out = torch.nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=2)
+		self.output_layer.add_module('out', out)
+		# Initializer
+		nn.init.normal(out.weight, mean=0.0, std=0.02)
+		nn.init.constant(out.bias, 0.0)
+		# Activation
+		self.output_layer.add_module('act', torch.nn.Tanh())
+
+	def forward(self, x):
+
+		x = x.view(x.size(0), -1)
+		x = self.linear(x)
+
+		h = self.hidden_layer(x.view(x.size(0), 512, 2, 2))
+		out = self.output_layer(h)
+		return out
+
+class Discriminator(torch.nn.Module):
+	def __init__(self, optimizer, lr, betas, batch_norm=False):
+		super(Discriminator, self).__init__()
+
+		self.projection = nn.utils.weight_norm(nn.Conv2d(1, 3, kernel_size=8, stride=2, padding=3, bias=False), name="weight")
+		self.projection.weight_g.data.fill_(1)
+
+		# Hidden layers
+		self.hidden_layer = torch.nn.Sequential()
+		num_filters = [64, 128, 256]
+		for i in range(3):
+			# Convolutional layer
+			if i == 0:
+				conv = nn.Conv2d(3, num_filters[i], kernel_size=4, stride=2, padding=2)
+			else:
+				conv = nn.Conv2d(num_filters[i - 1], num_filters[i], kernel_size=4, stride=2, padding=1)
+
+			conv_name = 'conv' + str(i + 1)
+			self.hidden_layer.add_module(conv_name, conv)
+
+			# Initializer
+			nn.init.normal(conv.weight, mean=0.0, std=0.02)
+			nn.init.constant(conv.bias, 0.0)
+
+			# Batch normalization
+			if i != 0 and batch_norm:
+				bn_name = 'bn' + str(i + 1)
+				self.hidden_layer.add_module(bn_name, torch.nn.BatchNorm2d(num_filters[i]))
+
+			# Activation
+			act_name = 'act' + str(i + 1)
+			self.hidden_layer.add_module(act_name, torch.nn.LeakyReLU(0.2))
+
+		# Output layer
+		self.output_layer = torch.nn.Sequential()
+		# Convolutional layer
+		out = nn.Conv2d(num_filters[i], 1, kernel_size=4, stride=1, padding=1)
+		self.output_layer.add_module('out', out)
+		# Initializer
+		nn.init.normal(out.weight, mean=0.0, std=0.02)
+		nn.init.constant(out.bias, 0.0)
+		# Activation
+		self.output_layer.add_module('act', nn.Sigmoid())
+
+		self.optimizer = optimizer(list(self.hidden_layer.parameters()) + list(self.output_layer.parameters()), lr=lr, betas=betas)
+
+	def forward(self, x):
+		p_x = self.projection(x)
+		h = self.hidden_layer(p_x)
+		out = self.output_layer(h)
+		return out
