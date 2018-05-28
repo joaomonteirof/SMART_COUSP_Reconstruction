686dc33e7515c763a3953c2a420f7f584f6b9227
joaomonteirof <joaomonteirof@gmail.com>
2018-05-28 19:54:37 -0400

    new train scheme inserted

--- models_zoo.py
+++ models_zoo.py
@@ -357,3 +357,146 @@ class model_3d_lstm(nn.Module):
 		x = F.sigmoid( self.fc( x.view(batch_size*seq_size, -1) ) )
 
 		return x.view(batch_size, seq_size, -1)
+
+class model_3d_lstm_gen(nn.Module):
+	def __init__(self, cuda_mode):
+		super(model_3d_lstm, self).__init__()
+
+		self.cuda_mode = cuda_mode
+
+		## Considering (30, 90) inputs
+
+		self.features = nn.Sequential(
+			nn.Conv2d(1, 16, kernel_size=(3,11), padding=1, stride=(1,2), bias=False),
+			nn.BatchNorm2d(16),
+			nn.ReLU(),
+			nn.Conv2d(16, 32, kernel_size=(3,9), padding=1, stride=(2,2), bias=False),
+			nn.BatchNorm2d(32),
+			nn.ReLU(),
+			nn.Conv2d(32, 64, kernel_size=(3,5), padding=1, stride=(2,2), bias=False),
+			nn.BatchNorm2d(64),
+			nn.ReLU(),
+			nn.ConvTranspose2d(64, 128, kernel_size=5, padding=2, stride=2, bias=False),
+			nn.BatchNorm2d(128),
+			nn.ReLU(),
+			nn.ConvTranspose2d(128, 96, kernel_size=5, padding=2, output_padding=1, stride=2, bias=False),
+			nn.BatchNorm2d(96),
+			nn.ReLU(),
+			nn.ConvTranspose2d(96, 64, kernel_size=3, padding=1, stride=1, bias=False),
+			nn.BatchNorm2d(64),
+			nn.ReLU(),
+			nn.ConvTranspose2d(64, 40, kernel_size=3, padding=1, stride=1, bias=False),
+			nn.BatchNorm2d(40),
+			nn.ReLU() )
+
+		self.res1 = res_block(1, 10)
+		self.res2 = res_block(1, 10)
+		self.res3 = res_block(1, 10)
+
+		self.out_conv = nn.Sequential( nn.ConvTranspose2d(40, 40, kernel_size=1, padding=0) )
+
+		self.lstm_1 = nn.LSTM(30*30, 30*30, 1, bidirectional=True, batch_first=False)
+
+		self.lstm_2 = nn.LSTM(2*30*30, 30*30, 1, bidirectional=True, batch_first=False)
+
+		self.fc = nn.Linear(2*30*30,100)
+
+	def forward(self, x):
+
+		x = self.features(x).unsqueeze(1)
+
+		x = self.res1(x)
+		x = self.res2(x)
+		x = self.res3(x)
+
+		x = x.view(x.size(0), x.size(2), x.size(3), x.size(4))
+
+		x = self.out_conv(x)
+
+		x = x.view(x.size(1), x.size(0), -1)
+
+		batch_size = x.size(1)
+		seq_size = x.size(0)
+
+		h0 = Variable(torch.zeros(2, batch_size, 30*30))
+		c0 = Variable(torch.zeros(2, batch_size, 30*30))
+
+		if self.cuda_mode:
+			h0 = h0.cuda()
+			c0 = c0.cuda()
+
+		x, h_c = self.lstm_1(x, (h0, c0))
+
+		x, _ = self.lstm_2(x, h_c)
+
+		x = F.sigmoid( self.fc( x.view(batch_size*seq_size, -1) ) )
+
+		return x.view(batch_size, seq_size, -1)
+
+class Generator(torch.nn.Module):
+	def __init__(self):
+		super(Generator, self).__init__()
+
+		#linear layer
+		self.linear = torch.nn.Sequential()
+
+		linear = nn.Linear(100, 2*2*512)
+
+		self.linear.add_module('linear', linear)
+
+		# Initializer
+		nn.init.normal(linear.weight, mean=0.0, std=0.02)
+		nn.init.constant(linear.bias, 0.0)
+
+		# Batch normalization
+		bn_name = 'bn0'
+		self.linear.add_module(bn_name, torch.nn.BatchNorm1d(2*2*512))
+
+		# Activation
+		act_name = 'act0'
+		self.linear.add_module(act_name, torch.nn.ReLU())
+
+		# Hidden layers
+		num_filters = [256, 128, 64]
+		self.hidden_layer = torch.nn.Sequential()
+		for i in range(3):
+			# Deconvolutional layer
+			if i == 0:
+				deconv = nn.ConvTranspose2d(512, num_filters[i], kernel_size=4, stride=2, padding=1)
+			else:
+				deconv = nn.ConvTranspose2d(num_filters[i - 1], num_filters[i], kernel_size=4, stride=2, padding=1)
+
+			deconv_name = 'deconv' + str(i + 1)
+			self.hidden_layer.add_module(deconv_name, deconv)
+
+			# Initializer
+			nn.init.normal(deconv.weight, mean=0.0, std=0.02)
+			nn.init.constant(deconv.bias, 0.0)
+
+			# Batch normalization
+			bn_name = 'bn' + str(i + 1)
+			self.hidden_layer.add_module(bn_name, torch.nn.BatchNorm2d(num_filters[i]))
+
+			# Activation
+			act_name = 'act' + str(i + 1)
+			self.hidden_layer.add_module(act_name, torch.nn.ReLU())
+
+		# Output layer
+		self.output_layer = torch.nn.Sequential()
+		# Deconvolutional layer
+		out = torch.nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=2)
+		self.output_layer.add_module('out', out)
+		# Initializer
+		nn.init.normal(out.weight, mean=0.0, std=0.02)
+		nn.init.constant(out.bias, 0.0)
+		# Activation
+		self.output_layer.add_module('act', torch.nn.Tanh())
+
+	def forward(self, x):
+
+		x = x.view(x.size(0), -1)
+		x = self.linear(x)
+
+		h = self.hidden_layer(x.view(x.size(0), 512, 2, 2))
+		out = self.output_layer(h)
+		return out
---
+++ train_gen.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+import argparse
+import torch
+import models_zoo
+from data_load import Loader, Loader_manyfiles
+from train_loop_adv import TrainLoop
+from torch.utils.data.dataloader import DataLoader
+import torch.optim as optim
+
+# Training settings
+parser = argparse.ArgumentParser(description='Online transfer learning for emotion recognition tasks')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
+parser.add_argument('--valid-batch-size', type=int, default=256, metavar='N', help='input batch size for testing (default: 512)')
+parser.add_argument('--epochs', type=int, default=200, metavar='N', help='number of epochs to train (default: 200)')
+parser.add_argument('--patience', type=int, default=10, metavar='N', help='How many epochs without improvement to wait before reducing the LR (default: 10)')
+parser.add_argument('--lr', type=float, default=0.0003, metavar='LR', help='learning rate (default: 0.0002)')
+parser.add_argument('--beta1', type=float, default=0.5, metavar='beta1', help='Adam beta 1 (default: 0.5)')
+parser.add_argument('--beta2', type=float, default=0.999, metavar='beta2', help='Adam beta 2 (default: 0.99)')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
+parser.add_argument('--input-data-path', type=str, default='./data/input/', metavar='Path', help='Path to data input data')
+parser.add_argument('--targets-data-path', type=str, default='./data/targets/', metavar='Path', help='Path to output data')
+parser.add_argument('--checkpoint-epoch', type=int, default=None, metavar='N', help='epoch to load for checkpointing. If None, training starts from scratch')
+parser.add_argument('--checkpoint-path', type=str, default=None, metavar='Path', help='Path for checkpointing')
+parser.add_argument('--generator-path', type=str, default=None, metavar='Path', help='Path for generator params')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+parser.add_argument('--save-every', type=int, default=5, metavar='N', help='how many batches to wait before logging training status. (default: 5)')
+parser.add_argument('--n-workers', type=int, default=2)
+args = parser.parse_args()
+args.cuda = True if not args.no_cuda and torch.cuda.is_available() else False
+
+#train_data_set = Loader(input_file=args.input_data_path+'input_train.hdf', output_file=args.targets_data_path+'output_train.hdf')
+train_data_set = Loader_manyfiles(input_file_base_name=args.input_data_path+'input_train', output_file_base_name=args.targets_data_path+'output_train', n_files=4)
+valid_data_set = Loader(input_file=args.input_data_path+'input_valid.hdf', output_file=args.targets_data_path+'output_valid.hdf')
+
+train_loader = DataLoader(train_data_set, batch_size=args.batch_size, shuffle=False, num_workers=args.n_workers)
+valid_loader = DataLoader(valid_data_set, batch_size=args.valid_batch_size, shuffle=False, num_workers=args.n_workers)
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+model = models_zoo.model_3d_lstm_gen(args.cuda)
+generator = models_zoo.Generator().eval()
+
+gen_state = torch.load(args.generator_path, map_location=lambda storage, loc: storage)
+generator.load_state_dict(mod_state['model_state'])
+
+if args.cuda:
+	model = model.cuda()
+	generator = generator.cuda()
+
+optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+
+trainer = TrainLoop(model, generator, optimizer, train_loader, valid_loader, checkpoint_path=args.checkpoint_path, checkpoint_epoch=args.checkpoint_epoch, cuda=args.cuda)
+
+print('Cuda Mode is: {}'.format(args.cuda))
+
+trainer.train(n_epochs=args.epochs, patience = args.patience, save_every = args.save_every)
