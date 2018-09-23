24acf60ade41542d391a61136cb996beb0df1dd9
joaomonteirof <joaomonteirof@gmail.com>
2018-09-23 12:36:24 -0400

    fix on data loader

--- data_load.py
+++ data_load.py
@@ -21,7 +21,7 @@ class Loader(Dataset):
 	def __getitem__(self, index):
 
 		if not self.in_file: self.in_file = h5py.File(self.input_file_name, 'r')
-		in_samples = self.in_file['input_samples'][:,:,index].squeeze().T
+		in_samples = self.in_file['data'][index]
 		in_samples = torch.from_numpy(in_samples).float().unsqueeze(0)
 
 		self.in_file.close()
@@ -55,7 +55,7 @@ class Loader_manyfiles(Dataset):
 		index = index % self.len_per_file
 
 		in_file = h5py.File(self.in_file_base_name+'_'+str(file_)+'.hdf', 'r')
-		in_samples = self.in_file['input_samples'][:,:,index].squeeze().T
+		in_samples = self.in_file['data'][index]
 		in_samples = torch.from_numpy(in_samples).float().unsqueeze(0)
 		in_file.close()
 
