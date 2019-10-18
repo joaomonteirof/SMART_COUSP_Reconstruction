625370835175d56cdb9d58c62f09d34d49759e21
Isabela <isabelamcalbuquerque@gmail.com>
2019-10-18 14:37:19 -0400

    repeating consecutive frames and script for visualization

--- output_data_gen.py
+++ output_data_gen.py
@@ -133,24 +133,31 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Generate Bouncing balls dataset')
 	parser.add_argument('--im-size', type=int, default=32, metavar='N', help='H and W of frames (default: 32)')
 	parser.add_argument('--n-balls', type=int, default=3, metavar='N', help='Number of bouncing balls (default: 3)')
-	parser.add_argument('--n-frames', type=int, default=128, metavar='N', help='Number of frames per sample (default: 128)')
-	parser.add_argument('--n-samples', type=int, default=500, metavar='N', help='Number of output samples (default: 500)')
+	parser.add_argument('--n-frames', type=int, default=25, metavar='N', help='Number of frames per sample (default: 128)')
+	parser.add_argument('--rep-times', type=int, default=4, metavar='N', help='Number of times consecutive frames are repeated (default: 4)')
+	parser.add_argument('--n-samples', type=int, default=50, metavar='N', help='Number of output samples (default: 500)')
 	parser.add_argument('--output-path', type=str, default='./', metavar='Path', help='Path for output')
 	parser.add_argument('--file-name', type=str, default='train.hdf', metavar='Path', help='Output file name')
 	args = parser.parse_args()
 
 	hdf = h5py.File(args.output_path+args.file_name, 'w')
+	
+	print('Number of frames: {}, repetitions: {}, total length: {}'.format(args.n_frames, args.rep_times, args.n_frames*args.rep_times))
 
 	for i in range(args.n_samples):
 		sample=bounce_mat(res=args.im_size, n=args.n_balls, T=args.n_frames)
-		dat = np.expand_dims( np.moveaxis(sample, 0, -1), axis=0 )
-
+		print(sample.shape)
+		sample = np.moveaxis(sample, 0, -1)
+		sample_repeated = np.repeat(sample, args.rep_times, axis=-1)
+		print(sample_repeated.shape)
+		dat = np.expand_dims(sample_repeated, axis=0)
+		print(dat.shape)
 		try:
 			hdf['data'].resize(hdf['data'].shape[0]+1, axis=0)
 			hdf['data'][-1:]=dat
 
 		except KeyError:
-			hdf.create_dataset('data', data=dat, maxshape=(None, args.im_size, args.im_size, args.n_frames))
+			hdf.create_dataset('data', data=dat, maxshape=(None, args.im_size, args.im_size, args.n_frames*args.rep_times))
 
 		print(i)
 
