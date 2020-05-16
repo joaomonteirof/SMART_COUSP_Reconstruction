43ba5a53fbff2d12b94fc2b8afb4fe5e26d495e7
joaomonteirof <joaomonteirof@gmail.com>
2020-05-16 19:55:58 -0400

    random mask

--- flying_numbers/train_loop.py
+++ flying_numbers/train_loop.py
@@ -73,7 +73,7 @@ class TrainLoop(object):
 				self.logger.add_scalar('Valid/Best_MSE', np.min(self.history['valid_loss']), self.total_iters)
 				grid_streaking = torchvision.utils.make_grid(input_streaking_images)
 				self.logger.add_image('Inputs', grid_streaking, self.total_iters)
-				z_ = torch.randn(x.size(0), 128).view(-1, 128, 1, 1).to(target_scenes.device)
+				z_ = torch.randn(8, 128).view(-1, 128, 1, 1).to(target_scenes.device)
 				grid_generator = torchvision.utils.make_grid(self.generator(z_))
 				self.logger.add_video('Reconstructed', torch.cat(frames_list, 1), self.total_iters)
 				self.logger.add_video('Target_scenes', target_scenes.permute(0,4,1,2,3), self.total_iters)
